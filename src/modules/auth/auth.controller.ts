5fcb1d79c5597c95b9db3fcb17acd9c4701c5cdf
otisdoan <ledoanhieu12a6@gmail.com>
2025-08-28 21:54:18 +0700

    format response api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRegisterDto } from './dto/register.dto';
-import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
-  async register(@Body() createUserDto: UserRegisterDto): Promise<User> {
-    return this.authService.register(createUserDto);
+  async register(@Body() createUserDto: UserRegisterDto) {
+    const user = await this.authService.register(createUserDto);
+    return {
+      status: 'success',
+      message: 'Đăng ký thành công',
+      data: user,
+    };
   }
 }
