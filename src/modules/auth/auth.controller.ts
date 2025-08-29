cc6f35837c05896dc52a45232baacf9b9bdbadd9
otisdoan <ledoanhieu12a6@gmail.com>
2025-08-29 14:47:35 +0700

    done api login, register

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserRegisterDto } from './dto/register.dto';
+import { UserLoginDto } from './dto/login.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
-  async register(@Body() createUserDto: UserRegisterDto) {
-    const user = await this.authService.register(createUserDto);
-    return {
-      status: 'success',
-      message: 'Đăng ký thành công',
-      data: user,
-    };
+  async register(@Body() createUserDto: UserRegisterDto, @Res() res: Response) {
+    try {
+      const user = await this.authService.register(createUserDto);
+
+      return res.status(HttpStatus.CREATED).json({
+        status: 'success',
+        message: 'Register successfully!',
+        data: user,
+      });
+    } catch (error) {
+      const err = error as Error;
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: UserLoginDto, @Res() res: Response) {
+    try {
+      const user = await this.authService.login(loginUserDto);
+
+      return res.status(HttpStatus.FOUND).json({
+        status: 'success',
+        message: 'Register successfully!',
+        data: user,
+      });
+    } catch (error) {
+      const err = error as Error;
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,12 +3,26 @@ import { UserRegisterDto } from './dto/register.dto';
 import { User } from './entities/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserLoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
   async register(createUserDto: UserRegisterDto): Promise<User> {
     const user = new this.userModel(createUserDto);
     return user.save();
+  }
+
+  async login(loginUserDto: UserLoginDto) {
+    const { email } = loginUserDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new Error('Email already exist!');
+    }
+
+    return user;
   }
 }
